a46ad7fcc1a299bda078df1277c48215fca7a1f3
bvsanusha <34126185+anushabvs@users.noreply.github.com>
2020-10-08 19:51:34 +0530

    Update Dataloader.py

--- Dataloader.py
+++ Dataloader.py
@@ -6,12 +6,15 @@ import scipy.io
 import cv2
 from PIL import Image
 from set_pixelfeature import set_pixelfeatures
-from feature_extractor import GOG_C1
+from GOG_C1_copy import GOG_C1
+import Feature_normalization
+import KLDA as k
 
 parser = argparse.ArgumentParser(description='Person ReID')
 
 parser.add_argument('--database', default = 0,type = int,help='Select a number from 0-7 to select a database')
 parser.add_argument('--feature_settings', default = 1,type = int,help=' 1-- GOG_RGB, 2 -- GOG_Fusion')
+parser.add_argument('--normalization', default = 1,type = int,help=' 1--normalization, 2 -- normalization2, 3 -- normalization_on_GOG')
 parser.set_defaults(database = 0)
 
 def set_default_parameter(lf_type):
@@ -33,7 +36,7 @@ def set_default_parameter(lf_type):
 	return param
 
 
-def main():
+def dataloader():
     global args
     args = parser.parse_args()
     setnum = 1
@@ -121,10 +124,12 @@ def main():
     elif args.database not in range(8):
         print("Database is not defined!!!")
     
-
+    #print(DBfile)
     Data = scipy.io.loadmat(DBfile)  
+    #print("All image names are: ",Data['allimagenames'].shape)
     all_image_names = Data['allimagenames']
     allimagenums = int(Data['allimagenames'].shape[0])
+    print('images',Data['traininds_set'][0][0].shape)
     #image size for resize
     H = 128
     W = 48
@@ -148,18 +153,19 @@ def main():
     print("Feature setting is ", args.feature_settings)
     for f in range(parFea.featurenum):
     	parFea.featureConf[f] = set_default_parameter(f)
+   
     ##########Feature Extraction########################
 
     print('*** low level feature extraction *** ')
     print('database = ', databasename)
-
     #Extracting features for all images
-    print('Extract feature for all images \n')
+    '''
     for f in range(parFea.featurenum):
     	if parFea.usefeature[f] == 1:
         	param = parFea.featureConf[f]
-                print(param.name)
+                #print(param.name)
                 feature_all = np.zeros((allimagenums, param.dimension))
+                #print(feature_all.shape)
                 t_0 = time.clock()
                 for imgind in range(allimagenums):
                         print("Processing image ",imgind)
@@ -167,20 +173,110 @@ def main():
                         	print('imging = ',imgind,allimagenums)
                         path = str(all_image_names[imgind])
                         path_final = os.path.join(datadirname,path[10:(len(path)-18)])
+                        print(path_final)
+                        #print(path[9:(len(path)-17)])
                         X = cv2.imread(path_final,1)
                         if X.shape != (H,W,3):
-                               X = cv2.resize(X,(W,H))
-                        
+                                #print("Resizing")
+                        	X = cv2.resize(X,(W,H))
+                        #print("Final_shape", X.shape,Y.shape)
 			feature_all[imgind,:] = GOG_C1(X)
-		
+		#print(feature_all)
 		t_1 = time.clock()
 		mean_time = t_1/allimagenums
 		print("Mean feature extraction time is {} seconds per image".format(round(mean_time,3)))
-		###Saving the extracted features###
-		#path_name = featuredirname + databasename+'_'+  param.name 
-                name = databasename+'_'+  param.name
+		#print("Feature all", param.name)
+		path_name = featuredirname + databasename+'_'+  param.name # change it to csv file or numpy array npy format
+                name = databasename+'_'+ 'feature_all_' + param.name
                 np.save(name,feature_all)
-              
+                
+    '''
+    ########KLDA_train##########
+    #Loading the features
+    print('*** load all extracted features ***')
+    feature_cell_all = np.zeros((parFea.featurenum, ), dtype=np.object)
+    for f in range(parFea.featurenum-1):
+        if parFea.usefeature[f] == 1:
+            print('feature = %d [ %s ] \n', f, parFea.featureConf[f].name)
+        
+        print('feature_all', parFea.featureConf[f].name);
+        name = '/home/user/Desktop/Person_ReID_python/' + databasename + '_'+ 'feature_all_'+ parFea.featureConf[f].name+'.npy'
+        print('Name ', name)
+        temp = np.load(name)
+        feature_cell_all[f] =temp
+
+    traininds_set = Data['traininds_set'][0] + 1
+    trainlabels_set = Data['trainlabels_set'][0]+ 1
+    testinds_set = Data['testinds_set'][0] + 1
+    testlabels_set = Data['testlabels_set'][0] + 1
+    traincamIDs_set = Data['traincamIDs_set'][0]
+    testcamIDs_set = Data['testcamIDs_set'][0]
+    #print("Data taken!!",np.size(Data['traininds_set']))
+    CMCs = np.zeros((setnum,numperson_garalley))
+    s=0
+    #Training data
+    print('Training Data!!!!')
+    tot = 1;
+    #extract feature cells for particular training/test division
+    feature_cell = np.zeros((parFea.featurenum, ), dtype=np.object)
+    if tot == 1:
+        numimages_train = np.size(traininds_set[s])
+        print('Number of training images are:',numimages_train)
+        for f in range(parFea.featurenum):
+            if parFea.usefeature[f] == 1:
+                feature_cell[f] = np.zeros(( numimages_train, feature_cell_all[f].shape[1]))
+                for ind in range (numimages_train):
+                    feature_cell[f][ind,:] = feature_cell_all[f][traininds_set[s][ind, :]]
+    print("This is done", feature_cell[0].shape)
+
+    if tot == 2:
+        numimages_test = np.size(testinds_set[s])
+        for f in range(parFea.featurenum):
+            if parFea.usefeature[f] == 1:
+                feature_cell[f] = np.zeros(( numimages_test, feature_cell_all[f].shape[1]))
+                for ind in range(numimages_test):
+                    feature_cell[f][ind,:] = feature_cell_all[f][testinds_set[s][ind, :]]
+    
+
+    d = Feature_normalization.feature_normalize()
+    #extract_feature_cell_from_all;  % load training data 
+    if(args.normalization==1):
+        features,meanX = d.apply_normalization(tot,parFea.usefeature,parFea.featurenum,feature_cell) #feature normalization
+    elif (args.normalization==2):
+        features,meanX = d.apply_normalization_onGoG(tot,parFea.usefeature,parFea.featurenum,feature_cell)
+    elif(args.normalization==3):
+        features,meanX = d.apply_normalization2(tot,parFea.usefeature,parFea.featurenum,feature_cell)
+    else:
+        print('Undefined normalization setting \n');
+        assert(False)
+    isfirst = 1
+    for f in range(parFea.featurenum):
+        if parFea.usefeature[f] == 1:
+            if isfirst == 1:
+                feature = features[f]
+                #print(feature.shape)
+                isfirst = 0
+            else:
+                feature2 = features[f]  
+                feature = np.hstack((feature,feature2))
   
-if __name__ == '__main__':
-    main()
+    #train NFST metric learning
+    camIDs = traincamIDs_set[s]
+    prob_1 = np.where(camIDs==1)
+    p = camIDs[prob_1].shape[0]
+    probX = feature[0:p, :]  
+    galX = feature[p:camIDs.shape[0], :]
+    labelsX = trainlabels_set[s]
+    probXLabels = labelsX[camIDs == 1]
+    galXLabels = labelsX[camIDs == 2]  
+    print("Done", probX.shape,galX.shape,probXLabels.shape,galXLabels.shape)
+
+    [K, alpha] = k.KLDA_train(probX, galX, probXLabels, galXLabels);
+    print('KLDA Training Ended Aplha Parameter Saved \n')
+    print('Training Completed \n')
+    alpha = np.squeeze(alpha)
+    np.save('alpha',alpha)
+    
+    ############Find Person######################
+    return alpha,Data,parFea	
+	
